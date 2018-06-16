75b5abf63e8123add420758c96d7b3df4e8701f9
Emanuele <emanuele.lamalfa@mail.polimi.it>
2018-06-16 13:00:41 +0200

    methods for clusters' initialization

--- models/clustering/kmeans/kmeans.py
+++ models/clustering/kmeans/kmeans.py
@@ -8,9 +8,14 @@ K-Means implementation
 """
 
 from distance import distance as dist
+from utils import cinit as cinit
 
+# dictionary to pretty-invoke distances' routines
 distances_dict = {'L2': dist.L2, 'L1': dist.L1, 'L0': dist.L0, 
-                  'LInf': dist.LInf}; 
+                  'LInf': dist.LInf};
+
+# dictionary to pretty-invoke clusters initialization's routines                  
+cluster_init_dict = {'random': cinit.random, 'kmeans++': cinit.kmeans_plus_plus};
 
 def kmeans(data, k=10, distance='L2'):
     
---
+++ models/clustering/kmeans/utils/cinit.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jun 16 12:56:13 2018
+
+@author: Emanuele
+
+Clusters initialization routines
+"""
+
+def random():
+    
+    pass;
+    
+def kmeans_plus_plus():
+    
+    pass;
