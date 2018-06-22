bed66b6023f5b7b0f155e26f0fec5c27a9a3b02f
Emanuele <emanuele.lamalfa@mail.polimi.it>
2018-06-22 16:36:40 +0200

    solve error in test distances

--- models/clustering/kmeans/test/test_distance.py
+++ models/clustering/kmeans/test/test_distance.py
@@ -22,38 +22,37 @@ if __name__ == "__main__":
      
     # create a vector where each entry has increasing number of ones
     x = np.array([[(1 if i>=j else 0) for i in range(10)] for j in range(10)]);
-    x = x.T;
     
     # L2
-    assert np.all(dist.L2(x, x[0]) == np.array([np.sqrt(i) for i in range(10)])), "L2 test has failed";
+    assert np.all(dist.L2(x[0], x) == np.array([np.sqrt(i) for i in range(10)])), "L2 test has failed";
     
     # L1
-    assert np.all(dist.L1(x, x[0]) == np.array([(i) for i in range(10)])), "L1 test has failed";
+    assert np.all(dist.L1(x[0], x) == np.array([(i) for i in range(10)])), "L1 test has failed";
     
     # L0
-    assert np.all(dist.L0(x, x[0]) == np.array([0 for i in range(10)])), "L0 test has failed";
+    assert np.all(dist.L0(x[0], x) == np.array([0 for i in range(10)])), "L0 test has failed";
     
     # LInf
-    assert np.all(dist.LInf(x, x[0]) == np.array([(1 if i!=0 else 0) for i in range(10)])), "LInf test has failed";
+    assert np.all(dist.LInf(x[0], x) == np.array([(1 if i!=0 else 0) for i in range(10)])), "LInf test has failed";
     
     # test dimensions
     
     x = np.random.rand(*(i for i in range(10, 30, 5)));
        
     # L2
-    assert len(dist.L2(x, x[0])) == (x.shape[0]), "L2 test has failed: dimensions mismatch";
-    assert (dist.L2(x, x[0]).ndim) == 1, "L2 test has failed: dimensions mismatch";
+    assert len(dist.L2(x[0], x)) == (x.shape[0]), "L2 test has failed: dimensions mismatch";
+    assert (dist.L2(x[0], x).ndim) == 1, "L2 test has failed: dimensions mismatch";
     
     # L1
-    assert len(dist.L1(x, x[0])) == (x.shape[0]), "L1 test has failed: dimensions mismatch";
-    assert (dist.L1(x, x[0]).ndim) == 1, "L1 test has failed: dimensions mismatch";
+    assert len(dist.L1(x[0], x)) == (x.shape[0]), "L1 test has failed: dimensions mismatch";
+    assert (dist.L1(x[0], x).ndim) == 1, "L1 test has failed: dimensions mismatch";
 
     
     # L0
-    assert len(dist.L0(x, x[0])) == (x.shape[0]), "L0 test has failed: dimensions mismatch";
-    assert (dist.L0(x, x[0]).ndim) == 1, "L0 test has failed: dimensions mismatch";
+    assert len(dist.L0(x[0], x)) == (x.shape[0]), "L0 test has failed: dimensions mismatch";
+    assert (dist.L0(x[0], x).ndim) == 1, "L0 test has failed: dimensions mismatch";
     
     # LInf
-    assert len(dist.LInf(x, x[0])) == (x.shape[0]), "LInf test has failed: dimensions mismatch";
-    assert (dist.LInf(x, x[0]).ndim) == 1, "LInf test has failed: dimensions mismatch";
+    assert len(dist.LInf(x[0], x)) == (x.shape[0]), "LInf test has failed: dimensions mismatch";
+    assert (dist.LInf(x[0], x).ndim) == 1, "LInf test has failed: dimensions mismatch";
 
