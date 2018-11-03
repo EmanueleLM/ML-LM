5a8b8db69e391fdf0481814165e40eb1a113cdd7
Emanuele <emanuele.lamalfa@mail.polimi.it>
2018-11-03 10:52:45 +0100

    sdar algorithm updated with test

--- models/MAP/sdem/sdar.py
+++ models/MAP/sdem/sdar.py
@@ -1,86 +1,81 @@
 # -*- coding: utf-8 -*-
 """
-Created on Sun Aug 19 15:29:27 2018
+Created on Sun Aug 19 15:44:09 2018
 
-@author: Emanuele
+@author: Gabriele
 
 Sequentially discounting Expectation Maximization algorithm, references
  "Yamanishi, Kenji, and Jun-ichi Takeuchi. "A unifying framework for detecting 
   outliers and change points from non-stationary time series data." 
   Proceedings of the eighth ACM SIGKDD international conference on Knowledge 
   discovery and data mining. ACM, 2002."
+  
 """
 
-import copy as cp
 import numpy as np
+import copy as cp
 
-# generalization of the one-dimensional (univariate) normal distribution to 
-#  higher dimensions
-def multivariate_gaussian(x, mean, cov):
-    
-    d = x.shape[0];
-     
-    # covariance matrices have positive (or null) determinant
-    det = np.linalg.det(cov);
-    inv = np.linalg.inv(cov);
-          
-    g_exp = (-1/2) * np.dot(np.dot((x-mean), inv), (x-mean));
-     
-    p_x = (np.exp(g_exp))/np.sqrt((2*np.pi)**d * det);
-     
-    return p_x;
+# sdar algorithm
+def sdar(X, discount = 0.1, k = 10):
 
-def SDAR(X, discount=.1, k=10):
+    d, n = X.shape
     
-    n_sample = X.shape[1];
+    # initialize covariance functions estimators
+    C = np.random.uniform(low=-1., high=1., size=(k, 1))   
+    C_prev = np.random.uniform(low=-1., high=1., size=(k, 1));     
     
-    # initialization: parameters 'mu_hat' and 'C_j'
-    mu_hat = np.zeros(shape=(X.shape[0])); 
-    C_j_prec = np.zeros(shape=(X.shape[0], k)); # previous window
-    C_j = np.zeros(shape=(X.shape[0], k));
-       
-    # parameters of the model
-    A = np.zeros(shape=(X.shape[0], k));
-    W = np.zeros(shape=(k,k));
+    # parameters of the model (used in eq. (6) of the paper)
+    weights = np.random.uniform(low=.0, high=1., size=(k, 1))
+    A = np.zeros(shape=(k, k)) 
     
-    # solutions to the algorithm
-    x_hat = np.zeros(shape=(X.shape[0], n_sample-k));
-    means_gaussian = np.zeros(shape=(X.shape[0], n_sample-k));
-    sigma = np.zeros(shape=(X.shape[0], X.shape[0], n_sample-k));
+    # bootstrap of the estimations of our model
+    mu_hat = np.zeros(shape=(1, d))      
+    sigma_hat = np.zeros(shape=(d,d))
     
-    for i in range(k, n_sample):
-            
-        mu_hat = (1-discount)*mu_hat + discount*X[:,i];
+    # list of results (last two equations after eq. (6) of the paper)
+    SIGMA_HAT = list()
+    X_HAT = list()
+    
+    for t in range (k, n):
         
-        # assign the new mean of the gaussian
-        means_gaussian[:,i] = mu_hat;
-        
-        for j in range(k):
+        mu_hat = (1-discount) * mu_hat + discount * X[:, t]
+               
+        for j in range (k):
+                    
+            C[j] = (1 - discount) * C[j] + discount * np.dot(X[:, t] - mu_hat, (X[:, t-j] - mu_hat).T) 
             
-            C_j[:,j] = (1-discount)*C_j[:,j] + discount*np.dot(X[:,i]-mu_hat, (X[:,i-j]-mu_hat).T);
+        window = np.concatenate((C_prev, C)).flatten()
+        
+        for a in range(k):
+            
+            A[a,:] = window[k-a-1:2*k-a-1]
          
-        # stack the old and new windows horizontaly:
-        #  the last element in C_j_prec is the first of C_j
-        window = np.hstack([C_j_prec[:,:-1], C_j]);
-            
-        for j in range(X.shape[0]):
-            
-            Y = C_j[j];
-            
-            for wind in range(k):
-                
-                W[wind] = window[j,1+wind:1+wind+k]; 
-            
-            A[j] = np.linalg.solve(W, Y);
-            
-        C_j_prec = cp.deepcopy(C_j);
+        weights = np.linalg.solve(A, C)
+          
+        # copy previous window into the new one
+        C_prev = cp.copy(C)
+    
+        # estimate next prediction  
+        X_hat = np.multiply(weights, (X[:,t-k:t]-mu_hat).T) + mu_hat
         
-        # predict the parameters for the next sample
-        x_hat[:,i] = np.sum(np.dot(A[j], X[:,i-k:i]-mu_hat), axis=1) + mu_hat;
-        # assign the new covariance matrix of the gaussian
-        sigma[:,:,i] = (1-discount)*sigma[:,:,max(0, i-1)] + discount*np.sum((X[:,i]-x_hat[:,i])**2);
+        sigma_hat = (1 - discount)*sigma_hat + discount*np.multiply((X[:,t].T-X_hat), (X[:,t].T-X_hat).T)
         
-    return means_gaussian, sigma;
+        # append estimations to the results
+        X_HAT.append(X_hat)
+        SIGMA_HAT.append(sigma_hat)
         
+    return X_HAT, SIGMA_HAT
         
 
+
+
+        
+        
+    
+        
+        
+            
+            
+    
+        
+        
